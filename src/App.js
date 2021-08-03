54d050b3bb7b6b1a4e7d79c17302764616de9436
Lam Chieu <lamchieudang@gmail.com>
2021-08-03 16:03:43 +0700

    add App

--- src/App.js
+++ src/App.js
@@ -23,3 +23,9 @@ function App() {
 }
 
 export default App;
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:lamchieu99/trello.git
+git push -u origin main
